126a626ba2e88e518b2f6643d50cb5955c24677c
Rico Saßen <r.sassen@neusta.de>
2013-08-29 15:44:27 +0200

    Extract command pattern version of light in a separate file.

--- open_close_principle/examples/command_pattern/light.js
+++ open_close_principle/examples/command_pattern/light.js
@@ -1,43 +1,7 @@
-/* The Invoker function */
-var Switch = function(){
-  var _commands = [];
-  this.storeAndExecute = function(command){
-    _commands.push(command);
-    command.execute();
-  }
-}
-
-/* The Receiver function */
-var Light = function(){
-  this.turnOn = function(){ console.log ('turn on')};
-  this.turnOff = function(){ console.log ('turn off') };
-}
-
-/* The Command for turning on the light - ConcreteCommand #1 */
-var FlipUpCommand = function(light){
-  this.execute = light.turnOn;
-}
-
-/* The Command for turning off the light - ConcreteCommand #2 */
-var FlipDownCommand = function(light){
-  this.execute = light.turnOff;
-}
-
-var light = new Light();
-var switchUp = new FlipUpCommand(light);
-var switchDown = new FlipDownCommand(light);
-var s = new Switch();
-
-s.storeAndExecute(switchUp);
-s.storeAndExecute(switchDown);
-
-
-
-
 var Light = function () {
   this.turnOn = function () { console.log ('turn on'); };
   this.turnOff = function () { console.log ('turn off'); };
-}
+};
 
 var light = new Light();
 light.turnOn();
